e8a9245ae10b69c1277359a559d66addec17c474
jeremias-2000 <jdossantospinheiro20@gmail.com>
2021-07-13 21:55:44 -0300

    conectando novamente ao MongoDB Atlas ,como também, iniciando a paginação e segurança da minha api

--- src/main/java/com/bookstore/Trabalho/Programacao3/TrabalhoProgramacao3Application.java
+++ src/main/java/com/bookstore/Trabalho/Programacao3/TrabalhoProgramacao3Application.java
@@ -2,10 +2,11 @@ package com.bookstore.Trabalho.Programacao3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableSwagger2
 @EnableCaching
 public class TrabalhoProgramacao3Application {
